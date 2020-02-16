e19702a98b37c7d01145dfe52956b5eafc6e06bb
Volodymyr Byno <volodymyr.byno@gmail.com>
2020-02-16 08:38:31 +0200

    Remote `update` functionality

--- app/controllers/keystroke_sessions_controller.rb
+++ app/controllers/keystroke_sessions_controller.rb
@@ -1,30 +1,18 @@
 class KeystrokeSessionsController < ApplicationController
   before_action :set_keystroke_session, only: [:show, :edit, :update, :destroy]
 
-  # GET /keystroke_sessions
-  # GET /keystroke_sessions.json
   def index
     @keystroke_sessions = KeystrokeSession.all
   end
 
-  # GET /keystroke_sessions/1
-  # GET /keystroke_sessions/1.json
   def show
   end
 
-  # GET /keystroke_sessions/new
   def new
     @keystroke_session = KeystrokeSession.new
   end
 
-  # GET /keystroke_sessions/1/edit
-  def edit
-  end
-
-  # POST /keystroke_sessions
-  # POST /keystroke_sessions.json
   def create
-    binding.pry
     @keystroke_session = KeystrokeSession.new(keystroke_session_params)
 
     respond_to do |format|
@@ -38,22 +26,6 @@ class KeystrokeSessionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /keystroke_sessions/1
-  # PATCH/PUT /keystroke_sessions/1.json
-  def update
-    respond_to do |format|
-      if @keystroke_session.update(keystroke_session_params)
-        format.html { redirect_to @keystroke_session, notice: 'Keystroke session was successfully updated.' }
-        format.json { render :show, status: :ok, location: @keystroke_session }
-      else
-        format.html { render :edit }
-        format.json { render json: @keystroke_session.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /keystroke_sessions/1
-  # DELETE /keystroke_sessions/1.json
   def destroy
     @keystroke_session.destroy
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :keystroke_sessions
+  resources :keystroke_sessions, only: %i[index new create show]
   root "keystroke_sessions#new"
 end
